c72934ab5ab75020f2a36a09ebfc7a87219c75f0
SAYAN MONDAL <91mondal.sayan@gmail.com>
2022-12-09 14:07:57 +0530

    Checkout added

--- app/src/main/java/com/example/plantonic/CheckOutOne.java
+++ app/src/main/java/com/example/plantonic/CheckOutOne.java
@@ -11,27 +11,15 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 public class CheckOutOne extends Fragment {
-    LinearLayout linearAddNewAddressBtn;
     View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view =  inflater.inflate(R.layout.fragment_check_out_one, container, false);
-        linearAddNewAddressBtn = view.findViewById(R.id.linearAddNewAddressBtn);
 
 
 
-        linearAddNewAddressBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
-                fragmentTransaction.setReorderingAllowed(true)
-                        .addToBackStack("address")
-                        .replace(R.id.fragmentContainerView, new AddressGivenFragment());
-                fragmentTransaction.commit();
-            }
-        });
         return view;
     }
 }
