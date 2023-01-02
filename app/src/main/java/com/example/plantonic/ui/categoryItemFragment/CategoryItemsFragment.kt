4f588300ef257d087e651bfa232427e9c4d7d6bf
SAYAN MONDAL <91mondal.sayan@gmail.com>
2023-01-02 16:30:58 +0530

    set adapter

--- app/src/main/java/com/example/plantonic/ui/categoryItemFragment/CategoryItemsFragment.kt
+++ app/src/main/java/com/example/plantonic/ui/categoryItemFragment/CategoryItemsFragment.kt
@@ -6,17 +6,26 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.plantonic.Adapter.PopularItemAdapter
+import com.example.plantonic.Adapter.listeners.OnProductListener
+import com.example.plantonic.R
 import com.example.plantonic.databinding.FragmentCategoryItemsBinding
+import com.example.plantonic.firebaseClasses.ProductItem
+import com.example.plantonic.ui.productDetailsScreen.ProductViewFragment
+import com.example.plantonic.utils.HomeUtil
 import com.example.plantonic.utils.constants.IntentConstants
 
-class CategoryItemsFragment : Fragment() {
+class CategoryItemsFragment : Fragment(), OnProductListener {
     private lateinit var binding :  FragmentCategoryItemsBinding
     private lateinit var viewModel : CategoryItemsFragmentViewModel
     private lateinit var categoryId : String
+    private lateinit var adapter: PopularItemAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,11 +35,18 @@ class CategoryItemsFragment : Fragment() {
         binding = FragmentCategoryItemsBinding.inflate(layoutInflater, container, false)
         viewModel = ViewModelProvider(this)[CategoryItemsFragmentViewModel::class.java]
         categoryId = arguments?.getString(IntentConstants.CATEGORY_ID).toString()
+        adapter = PopularItemAdapter(requireContext(), this)
 
+        //Initializing recyclerView
+        binding.categoryItemsRecyclerView.layoutManager = GridLayoutManager(requireContext(),2)
+
+        //set adapter
+        binding.categoryItemsRecyclerView.adapter = this.adapter
         viewModel.getCategoriesItems(categoryId)
 
         viewModel.allCategories.observe(viewLifecycleOwner, Observer {
             Log.d("categoryItems", it.toString())
+            adapter.updatePopularProducts(ArrayList(it))
         })
 
 
@@ -62,5 +78,18 @@ class CategoryItemsFragment : Fragment() {
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    override fun onProductClick(productItem: ProductItem?) {
+        val productViewFragment = ProductViewFragment()
+        val bundle = Bundle()
+        bundle.putString(IntentConstants.PRODUCT_ID, productItem?.productId)
+        productViewFragment.arguments = bundle
+        val fragmentTransaction = parentFragmentManager.beginTransaction()
+        fragmentTransaction
+            .setReorderingAllowed(true)
+            .addToBackStack("detailsScreen")
+            .replace(R.id.fragmentContainerView, productViewFragment)
+        fragmentTransaction.commit()
     }
 }
